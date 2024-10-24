1850ba2f894874ff360e76d59e429402bfd67dad
castaneai <castaneai@castaneai.net>
2024-10-24 09:56:18 +0900

    fix

--- src/routes/find.ts
+++ src/routes/find.ts
@@ -19,12 +19,23 @@ export const handler: Handlers<Emoji[]> = {
 }
 
 async function searchEmojis(kv: Deno.Kv, query: string): Promise<Emoji[]> {
-    return (await kv.getMany(await searchEmojiKeys(query)))
-        .map(r => r.value as Emoji)
+    let results: Emoji[] = []
+    // The maximum number of keys given to kv.getMany is 10
+    for (const keys of chunks(await searchEmojiKeys(query), 10)) {
+        results = results.concat((await kv.getMany(keys, {consistency: "eventual"})).map(r => r.value as Emoji))
+    }
+    return results
 }
 
 async function searchEmojiKeys(query: string): Promise<string[][]> {
-    return Object.keys(await thoth.search(query)).map(k => ["emojis", k])
+    const emojis = Object.keys(await thoth.search(query))
+    return Array.from(new Set(emojis)).map(k => ["emojis", k])
+}
+
+function chunks<T>(array: T[], size: number): T[][] {
+    return Array.from({ length: Math.ceil(array.length / size) }, (_, i) =>
+      array.slice(i * size, (i + 1) * size),
+    )
 }
 
 const dummyResponse = JSON.parse(`[{"title":"😀","keywords":["grinning","face","grin"],"imageUrl":"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Noto_Emoji_KitKat_1f600.svg/512px-Noto_Emoji_KitKat_1f600.svg.png"},{"title":"😁","keywords":["beaming","face","with","smiling","eyes","eye","grin","smile"],"imageUrl":"https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Noto_Emoji_KitKat_1f601.svg/512px-Noto_Emoji_KitKat_1f601.svg.png"},{"title":"😂","keywords":["face","with","tears","of","joy","laugh","tear"],"imageUrl":"https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Noto_Emoji_KitKat_1f602.svg/512px-Noto_Emoji_KitKat_1f602.svg.png"}]`)
